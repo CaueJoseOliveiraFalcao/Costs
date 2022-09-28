5c231e9f66eeb4c3426f359025f272d8ea530a85
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-09-28 14:08:01 -0300

    gg

--- app/src/pages/NewProject.jsx
+++ app/src/pages/NewProject.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './style.css'
+import style from './NewProject.module.css'
 export default function NewProject(){
     return(
         <section className='fix-position'>
-            NewProject
+            <div className={style.divP}>
+                <h1>Criar Projeto</h1>
+                <p>Crie seu projeto para depois adiconar os serviços</p>
+                <p>Formulario</p>
+            </div>
         </section>
     )
 }
